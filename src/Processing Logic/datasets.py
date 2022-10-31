77e16758843631258d52a341d69125fd0e5571d8
htefera <haftish.birhan@gmail.com>
2022-10-31 10:03:20 +0100

    Added data processing logic

--- src/Processing Logic/datasets.py
+++ src/Processing Logic/datasets.py
@@ -5,7 +5,9 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.ml.feature import VectorAssembler
 
-def get_titanic(source):
+sc = SparkContext().getOrCreate()
+
+def get_titanic(source="train.csv"):
 	titanic = pd.read_csv(source)
 	titanic.drop(columns="Cabin", inplace=True)
 	
@@ -26,7 +28,7 @@ def get_titanic(source):
 	titanic["Sex"] = np.where(titanic["Sex"] == "male", 1, 0)
 	titanic["Ticket"].replace(list(titanic["Ticket"].value_counts().index), range(len(titanic["Ticket"].value_counts())), inplace=True)
 	titanic["Embarked"].replace(list(titanic["Embarked"].value_counts().index), range(len(titanic["Embarked"].value_counts())), inplace=True)
-	add = titanic.sum(axis=1)
+	add = titanic.drop(columns="Survived").sum(axis=1)
 	titanic = titanic[(add < add.quantile(0.9)) & (add > add.quantile(0.1))]
 	titanic = pd.concat([titanic[titanic['Survived'] == 0].sample(len(titanic[titanic['Survived'] == 1])),\
                      titanic[titanic['Survived'] == 1]], axis=0)
@@ -36,6 +38,39 @@ def get_titanic(source):
 		titanic[i] = (titanic[i] - titanic[i].mean()) / titanic[i].std()
 	return titanic
     
+def get_diabetes(source="diabetes.csv", seed=0):
+	diabetes = pd.read_csv(source)
+	col = list(diabetes.columns)
+	col.remove("Outcome")
+	for i in col:
+		diabetes[i] = (diabetes[i] - diabetes[i].mean()) / diabetes[i].std()
+	add = diabetes.drop(columns="Outcome").sum(axis=1)
+	diabetes = diabetes[(add < add.quantile(0.95)) & (add > add.quantile(0.05))]
+	diabetes = pd.concat([diabetes[diabetes['Outcome'] == 0].sample(len(diabetes[diabetes['Outcome'] == 1]), \
+                    random_state=seed), diabetes[diabetes['Outcome'] == 1]], axis=0)
+	return diabetes
+
+def get_fashion(source="fashion-mnist_train.csv"):
+	fashion = pd.read_csv(source)
+	#fashion = fashion[:1000]
+	col = list(fashion.columns)
+	col.remove("label")
+	for i in col:
+		if ((fashion[i]>0).sum())<25000:
+		    fashion.drop(columns=i, inplace=True)
+	fashion.drop(index=fashion[fashion.drop(columns="label").duplicated()].index, inplace=True)
+	col = list(fashion.columns)
+	col.remove("label")
+	for i in col:
+		fashion[i] = fashion[i] / 255
+	new = fashion.iloc[0:2]
+	new.drop(index=[0,1], inplace=True)
+	for i in range(10):
+		add = fashion[fashion["label"]==i].drop(columns="label").sum(axis=1)
+		new = pd.concat([new, fashion[fashion["label"]==i][(add < add.quantile(0.99)) & (add > add.quantile(0.01))]], axis=0)
+	fashion = new.sort_index()
+	return fashion
+
 def get_sklearn_titanic(source="train.csv", test_size=0.2, seed=0):
 	titanic = get_titanic(source)
 	titanic_label = titanic["Survived"]
@@ -45,8 +80,6 @@ def get_sklearn_titanic(source="train.csv", test_size=0.2, seed=0):
 def get_pyspark_titanic(source="train.csv", test_size=0.2, seed=0):
 	titanic = get_titanic(source)
 	titanic = titanic.rename(columns={'Survived': 'label'})
-    del sc
-	sc = SparkContext.getOrCreate()
 	sqlContext = SQLContext(sc)
 	data = sqlContext.createDataFrame(titanic)
 	col = list(titanic.columns)
@@ -56,3 +89,37 @@ def get_pyspark_titanic(source="train.csv", test_size=0.2, seed=0):
 	va_df = va_df.select(['features', 'label'])
 	return va_df.randomSplit([1-test_size, test_size], seed=seed)
 	
+def get_sklearn_diabetes(source="diabetes.csv", test_size=0.2, seed=0):
+	diabetes = get_diabetes(source)
+	diabetes_label = diabetes["Outcome"]
+	diabetes.drop(columns="Outcome", inplace=True)
+	return train_test_split(diabetes, diabetes_label, test_size=test_size, random_state=seed)
+	
+def get_pyspark_diabetes(source="diabetes.csv", test_size=0.2, seed=0):
+	diabetes = get_diabetes(source)
+	diabetes = diabetes.rename(columns={'Outcome': 'label'})
+	sqlContext = SQLContext(sc)
+	data = sqlContext.createDataFrame(diabetes)
+	col = list(diabetes.columns)
+	col.remove("label")
+	va = VectorAssembler(inputCols = col, outputCol='features')
+	va_df = va.transform(data)
+	va_df = va_df.select(['features', 'label'])
+	return va_df.randomSplit([1-test_size, test_size], seed=seed)
+
+def get_sklearn_fashion(source="fashion-mnist_train.csv", test_size=0.2, seed=0):
+	fashion = get_fashion(source)
+	fashion_label = fashion["label"]
+	fashion.drop(columns="label", inplace=True)
+	return train_test_split(fashion, fashion_label, test_size=test_size, random_state=seed)
+	
+def get_pyspark_fashion(source="fashion-mnist_train.csv", test_size=0.2, seed=0):
+	fashion = get_fashion(source)
+	sqlContext = SQLContext(sc)
+	data = sqlContext.createDataFrame(fashion)
+	col = list(fashion.columns)
+	col.remove("label")
+	va = VectorAssembler(inputCols = col, outputCol='features')
+	va_df = va.transform(data)
+	va_df = va_df.select(['features', 'label'])
+	return va_df.randomSplit([1-test_size, test_size], seed=seed)
